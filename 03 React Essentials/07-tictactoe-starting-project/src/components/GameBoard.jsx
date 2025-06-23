e9f6d571292b88b494cb97b9bd14906e654878a3
JH0908 <mashifed@gmail.com>
2025-06-23 18:39:14 +0900

    study ~86

--- 03 React Essentials/07-tictactoe-starting-project/src/components/GameBoard.jsx
+++ 03 React Essentials/07-tictactoe-starting-project/src/components/GameBoard.jsx
@@ -1,21 +1,27 @@
-import { useState } from 'react';
 const initialGameBoard = [
     [null, null, null],
     [null, null, null],
-    [null, null, null],
+    [null, null, null]
 ];
 
-export default function GameBoard() {
-    const [gameBoard, setGameBoard] = useState([initialGameBoard]);
-console.log(initialGameBoard);
-    console.log(gameBoard);
+export default function GameBoard({ onSelectSquare, turns }) {/*activePlayerSymbol*/
+    let gameBoard = initialGameBoard;
+
+    for (const turn of turns) {
+        const { square, player } = turn;
+        const { row, col } = square;
+
+        gameBoard[row][col] = player;
+    }
+    /*const [gameBoard, setGameBoard] = useState(initialGameBoard);
     function handleSelectSquare(rowIndex, colIndex) {
         setGameBoard((prevGameBoard) => {
             const updatedBoard = [...prevGameBoard.map(innerArray => [...innerArray])];
-            updatedBoard[rowIndex][colIndex] = 'X';
+            updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
             return updatedBoard;
         });
-    }
+        onSelectSquare();
+    }*/
     return (
         <ol id="game-board">
             {gameBoard.map((row, rowIndex) => (
@@ -23,7 +29,7 @@ console.log(initialGameBoard);
                     <ol>
                         {row.map((playerSymbol, colIndex) => (
                             <li key={colIndex}>
-                                <button onClick={() => handleSelectSquare(rowIndex, colIndex)}>{playerSymbol}</button>
+                                <button onClick={() => onSelectSquare(rowIndex, colIndex)}>{playerSymbol}</button>
                             </li>
                         ))}
                     </ol>
