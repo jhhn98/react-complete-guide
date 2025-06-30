058e4971a25c8f814e6980784771b1f9b5f8ee5e
JH0908 <mashifed@gmail.com>
2025-06-30 16:27:37 +0900

    study ~104

--- 05-Essential-Practice/01-starting-project/src/App.jsx
+++ 05-Essential-Practice/01-starting-project/src/App.jsx
@@ -1,7 +1,34 @@
+import {Fragment, useState} from "react";
+import Header from './components/Header';
+import UserInput from './components/UserInput';
+import Results from "./components/Results";
 function App() {
-  return (
-    <h1>React Investment Calculator</h1>
-  )
+    const [userInput, setUserInput] = useState({
+        initialInvestment: 10000,
+        annualInvestment: 1200,
+        expectedReturn: 6,
+        duration: 10
+    });
+
+    const inputIsValid = userInput.duration >= 1;
+
+    function handleChange(inputIdentifier, newValue) {
+        setUserInput(prevUserInput => {
+            return {
+                ...prevUserInput,
+                [inputIdentifier]: +newValue
+            };
+        });
+    }
+
+    return (
+        <Fragment>
+            <Header/>
+            <UserInput onChange={handleChange} userInput={userInput} />
+            {!inputIsValid && <p className="center">Please enter a duration greater than 0.</p>}
+            {inputIsValid && <Results input={userInput} />}
+        </Fragment>
+    )
 }
 
 export default App
