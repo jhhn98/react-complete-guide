94aeb933283628ea0bb5e33a1eee78715faa1c49
jh <jhhn0908@gmail.com>
2025-07-20 23:37:06 +0900

    study ~ 152, 153

--- 09-Project-Manager/01-starting-project/src/components/NewProject.jsx
+++ 09-Project-Manager/01-starting-project/src/components/NewProject.jsx
@@ -22,12 +22,12 @@ export default function NewProject({ onAdd }) {
         <div className="w-[35rem] mt-16">
             <menu className="flex items-center justify-end gap-4 my-4">
                 <li><button className="text-stone-800 hover:text-stone-950">Cancel</button></li>
-                <li><button className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950">Save</button></li>
+                <li><button onClick={handleSave} className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950">Save</button></li>
             </menu>
             <div>
-                <Input type="text" label="Title" />
-                <Input label="Description" textarea />
-                <Input type="date" label="Due Date" />
+                <Input ref={title} type="text" label="Title" />
+                <Input ref={description} label="Description" textarea />
+                <Input ref={dueDate } type="date" label="Due Date" />
             </div>
         </div>
     )
